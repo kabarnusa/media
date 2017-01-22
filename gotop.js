5af7c959e42ca42d836d9db5a8ec32765df2fda7
kabarnusa.com <kabarnusacom@gmail.com>
2017-01-22 10:48:09 +0700

    Add files via upload

---
+++ gotop.js
@@ -0,0 +1,2 @@
+$(function() { $(window).scroll(function() { if($(this).scrollTop()&gt;500) { $(&#39;.gotop&#39;).slideDown(200); } else { $(&#39;.gotop&#39;).slideUp(300); } });
+$(&#39;.gotop&#39;).click(function() { $(&#39;body,html&#39;).animate({scrollTop:0},800) .animate({scrollTop:25},200) .animate({scrollTop:0},150) .animate({scrollTop:10},100) .animate({scrollTop:0},50); }); });
